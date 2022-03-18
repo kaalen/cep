7a05ade989a040dc190f21a50a81e6506fd4a3be
Alenka Caserman <alenka.caserman@gmail.com>
2022-03-18 20:22:26 +1000

    added label property to TrackableObject

--- examples/cait/trackable_object.py
+++ examples/cait/trackable_object.py
@@ -1,10 +1,11 @@
 # from https://www.pyimagesearch.com/2018/08/13/opencv-people-counter/
 class TrackableObject:
-    def __init__(self, objectID, centroid):
+    def __init__(self, objectID, centroid, label):
         # store the object ID, then initialize a list of centroids
         # using the current centroid
         self.objectID = objectID
         self.centroids = [centroid]
+        self.label = label
 
         # initialize a boolean used to indicate if the object has
         # already been counted or not
